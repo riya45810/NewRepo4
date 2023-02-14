e5216197060facbd2b1f86f2f507cfba70292b93
riya.fathima <riya.fathima@RIYAFATHIMA>
2023-02-14 17:25:07 +0530

    Add project files.

---
+++ Reverse4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name: ");
+            Console.WriteLine("Enter name: ");
+            string name = Console.ReadLine();
+            string rev = string.Empty;
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                rev += name[i];
+            }
+
+            Console.WriteLine($"Reverse is {rev}");
+
+
+
+        }
+    }
+}
