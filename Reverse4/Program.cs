a810fe419718d49c0b472fa1e7d0217c3d78030c
riya45810 <riya.fathima@experionglobal.com>
2023-02-15 14:08:13 +0530

    again committed

--- Reverse4/Program.cs
+++ Reverse4/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the name: ");
-            Console.WriteLine("Enter name: ");
+            Console.WriteLine("Enter the string: "); 
+            
             string name = Console.ReadLine(); // comment
             string rev = string.Empty;
             for (int i = name.Length - 1; i >= 0; i--)
@@ -13,7 +13,7 @@
                 rev += name[i];//comment
             }
 
-            Console.WriteLine($"Reverse is {rev}");
+            Console.WriteLine($"Reverse string is {rev}");
 
 
 
